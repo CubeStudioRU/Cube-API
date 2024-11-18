f462249145c62079929f7c4b4b1d5cad81dc5279
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-11-18 13:38:21 +0400

    updated endpoint

--- app/api/v1/instances/instances.py
+++ app/api/v1/instances/instances.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
-from app.compile import is_compiled_instance_up_to_date, get_compiled_instance, compile_instance
-from app.schemas import CompiledInstance
+from app.schemas.instance_schema import CompiledInstance
+from app.services.compile_service import CompileService
 
 instances_router = APIRouter(
     prefix='/instances',
@@ -11,13 +11,4 @@ instances_router = APIRouter(
 
 @instances_router.get('/')
 async def get_instance() -> CompiledInstance:
-    is_instance_compiled: bool = await is_compiled_instance_up_to_date()
-    if is_instance_compiled is None:
-        return None
-
-    if not is_instance_compiled:
-        response = await compile_instance()
-        if not response:
-            return None
-
-    return await get_compiled_instance()
+    return await CompileService.get_compiled_instance()
