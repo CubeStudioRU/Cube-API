b719462b4b975c41b1920a491e5761906edf9074
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-11-18 22:10:40 +0400

    added factory to instance_service

--- app/services/instance_service.py
+++ app/services/instance_service.py
@@ -4,6 +4,12 @@ from app.schemas.instance_schema import Instance
 
 
 class InstanceService:
-    @staticmethod
-    async def get_instance() -> Instance:
+    def __init__(self):
+        pass
+
+    async def get_instance(self) -> Instance:
         return Instance.model_validate(get_json(INSTANCE_FILE))
+
+
+async def instance_service_factory() -> InstanceService:
+    return InstanceService()
