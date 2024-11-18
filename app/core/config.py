236b79dad3ad3c8d3626331c2fcbeb33c2550b2c
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-11-18 13:38:03 +0400

    moved back from paths

--- app/core/config.py
+++ app/core/config.py
@@ -1,12 +1,13 @@
-from dotenv import load_dotenv, dotenv_values
 import os
+
+from dotenv import load_dotenv
 
 load_dotenv('.env')
 
 API_KEY = os.getenv('API_KEY')
 CURSEFORGE_API_KEY = os.getenv('CURSEFORGE_API_KEY')
 
-COMPILED_INSTANCE_FILE = './compiled_instance.json'
+COMPILED_INSTANCE_FILE = './instance/compiled_instance.json'
 INSTANCE_FILE = './instance/instance.json'
 
 BASE_CURSEFORGE_URL = 'https://api.curseforge.com/v1'
