c251cc7bd5432ca1c4865d9f5f95d8ca27978617
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-11-23 17:43:00 +0400

    added mods side counting

--- app/integrations/base_integration.py
+++ app/integrations/base_integration.py
@@ -1,8 +1,8 @@
 from abc import ABC, abstractmethod
 from typing import List
 
-from app.schemas.instance_schema import Instance
-from app.schemas.mod_schema import BaseMod, CompiledInstanceMod
+from app.schemas.instance_schema import Instance, InstanceType
+from app.schemas.mod_schema import BaseMod, CompiledInstanceMod, ModSide
 
 
 class BaseIntegration(ABC):
@@ -12,8 +12,20 @@ class BaseIntegration(ABC):
     async def get_mod(self, mod: BaseMod) -> CompiledInstanceMod:
         pass
 
-    async def get_mods(self, mods: List[BaseMod]) -> List[CompiledInstanceMod]:
-        return [await self.get_mod(mod) for mod in mods]
+    async def get_mods(self, mods: List[BaseMod], instance_type: InstanceType) -> List[CompiledInstanceMod]:
+        supported_mods = [ModSide.both]
+        if instance_type == InstanceType.client:
+            supported_mods.append(ModSide.client)
+        elif instance_type == InstanceType.server:
+            supported_mods.append(ModSide.server)
+
+        compiled_mods = []
+
+        for mod in mods:
+            if mod.side in supported_mods:
+                compiled_mods.append(await self.get_mod(mod))
+
+        return compiled_mods
 
     @abstractmethod
     async def extract_mods(self, instance: Instance) -> List[BaseMod]:
