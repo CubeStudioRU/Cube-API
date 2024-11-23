2fc71e5919a350ec06df00b9f5bc6597179e9b4e
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-11-23 17:51:38 +0400

    refactored FileCacheService to JsonVaultCacheService

--- app/core/config.py
+++ app/core/config.py
@@ -7,8 +7,7 @@ load_dotenv('.env')
 API_KEY = os.getenv('API_KEY')
 CURSEFORGE_API_KEY = os.getenv('CURSEFORGE_API_KEY')
 
-COMPILED_CLIENT_INSTANCE_FILE = './instance/compiled_client_instance.json'
-COMPILED_SERVER_INSTANCE_FILE = './instance/compiled_server_instance.json'
+COMPILED_INSTANCE_VAULT = './instance/'
 INSTANCE_FILE = './instance/instance.json'
 
 BASE_CURSEFORGE_URL = 'https://api.curseforge.com/v1'
--- app/services/cache_service.py
+++ app/services/cache_service.py
@@ -1,9 +1,9 @@
 from abc import abstractmethod
 
-from app.core.config import COMPILED_CLIENT_INSTANCE_FILE
+from app.core.config import COMPILED_INSTANCE_VAULT
 from app.core.utils import hash_dict
 from app.crud.files import get_json, save_json
-from app.schemas.instance_schema import CompiledInstance, Instance
+from app.schemas.instance_schema import CompiledInstance, Instance, InstanceType
 
 
 class CacheService:
@@ -16,39 +16,40 @@ class CacheService:
         pass
 
 
-class FileCacheService(CacheService):
-    def __init__(self, file_path: str):
-        self.file_path = file_path
+class JsonVaultCacheService(CacheService):
+    def __init__(self, vault_path: str):
+        self.vault_path = vault_path
 
     async def get(self, key: str) -> dict | None:
-        data = get_json(self.file_path)
+        data = get_json(self.vault_path + f"/{key}.json")
         return data
 
     async def set(self, key: str, value: dict) -> None:
-        save_json(value, self.file_path)
+        save_json(value, self.vault_path + f"/{key}.json")
 
 
 class CompileCacheService:
     def __init__(self, cache_service: CacheService):
         self.cache_service = cache_service
 
-    async def get_valid_cached_instance(self, instance: Instance) -> CompiledInstance | None:
-        cached_instance = await self.get_cached_instance()
+    async def get_valid_cached_instance(self, instance: Instance,
+                                        instance_type: InstanceType) -> CompiledInstance | None:
+        cached_instance = await self.get_cached_instance(instance_type)
         if cached_instance and hash_dict(instance.model_dump()) == cached_instance.instance_hash:
             return cached_instance
         return None
 
-    async def get_cached_instance(self) -> CompiledInstance | None:
-        data = await self.cache_service.get("instance")
+    async def get_cached_instance(self, instance_type: InstanceType) -> CompiledInstance | None:
+        data = await self.cache_service.get(instance_type.name)
         if data is None:
             return None
         return CompiledInstance.model_validate(data)
 
-    async def update_cached_instance(self, instance: CompiledInstance):
-        await self.cache_service.set("instance", instance.model_dump())
+    async def update_cached_instance(self, instance: CompiledInstance, instance_type: InstanceType):
+        await self.cache_service.set(instance_type.name, instance.model_dump())
 
 
 async def compile_cache_service_factory(cache_service: CacheService = None) -> CompileCacheService:
     if cache_service is None:
-        cache_service = FileCacheService(COMPILED_CLIENT_INSTANCE_FILE)
+        cache_service = JsonVaultCacheService(COMPILED_INSTANCE_VAULT)
     return CompileCacheService(cache_service)
