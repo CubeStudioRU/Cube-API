f0c8fcf7539072e0162d0e2a60ab7fccf76e2f17
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-11-24 13:13:31 +0400

    fully refactored cache service

--- app/services/cache_service.py
+++ app/services/cache_service.py
@@ -1,25 +1,31 @@
-from abc import abstractmethod
+from pydantic import BaseModel
+from typing_extensions import TypeVar
 
-from app.crud.files import get_json, save_json
+from app.core.utils import hash_dict
+from app.repositories.cache_repository import CacheRepository
+from app.schemas.cache_schema import Cached
 
-
-class CacheService:
-    @abstractmethod
-    async def get(self, key: str) -> dict | None:
-        pass
-
-    @abstractmethod
-    async def set(self, key: str, value: dict) -> None:
-        pass
+Entity = TypeVar("Entity", bound=BaseModel)
+CachedEntity = TypeVar("CachedEntity", bound=Cached)
 
 
-class JsonVaultCacheService(CacheService):
-    def __init__(self, vault_path: str):
-        self.vault_path = vault_path
+class CacheService(Entity, CachedEntity):
+    def __init__(self, repository: CacheRepository):
+        self.repository = repository
 
-    async def get(self, key: str) -> dict | None:
-        data = get_json(self.vault_path + f"/{key}.json")
-        return data
+    async def get_valid_cache(self, entity: Entity, entity_hash: str) -> CachedEntity | None:
+        cached = await self.get_cache(entity_hash)
+        if cached and hash_dict(entity.model_dump()) == cached.hash:
+            return cached
+        return None
 
-    async def set(self, key: str, value: dict) -> None:
-        save_json(value, self.vault_path + f"/{key}.json")
+    async def get_cache(self, entity_hash: str) -> CachedEntity | None:
+        return await self.repository.get(entity_hash)
+
+    async def delete_cache(self, entity_hash: str) -> None:
+        cached = await self.get_cache(entity_hash)
+        if cached:
+            await self.repository.delete(cached)
+
+    async def update_cache(self, cached_entity: CachedEntity):
+        await self.repository.save(cached_entity)
