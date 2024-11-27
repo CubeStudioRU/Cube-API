9e0b1b952e7ad03b6b6361d8c6ab0cd9a6480e33
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-11-27 18:19:46 +0400

    fixed

--- app/repositories/mod_repository.py
+++ app/repositories/mod_repository.py
@@ -8,6 +8,6 @@ from app.schemas.mod_schema import IntegrationMod, IntegrationType, ModSide
 class ModRepository(BaseRepository[IntegrationMod], ABC):
     repository_name = "mods"
 
-    def get_by_integration_and_sides(self, integration_type: IntegrationType, mod_sides: List[ModSide]) -> List[
+    async def get_by_integration_and_sides(self, integration_type: IntegrationType, mod_sides: List[ModSide]) -> List[
         IntegrationMod]:
         pass
