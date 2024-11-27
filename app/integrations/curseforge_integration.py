4863e833a4cdf68a8782b3cdc8a24d0bec2432c8
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-11-27 18:14:47 +0400

    refactored along new Integration mod

--- app/integrations/curseforge_integration.py
+++ app/integrations/curseforge_integration.py
@@ -1,12 +1,9 @@
-from typing import List
-
 import aiohttp
 from fastapi.params import Depends
 
 from app.core.config import get_settings
 from app.integrations.base_integration import BaseIntegration
-from app.schemas.instance_schema import Instance
-from app.schemas.mod_schema import CurseforgeMod, CompiledMod, IntegrationMod
+from app.schemas.mod_schema import IntegrationMod, CompiledMod, IntegrationType
 from app.services.mod_cache_service import ModCacheService, get_mod_cache_service
 
 
@@ -14,8 +11,8 @@ class CurseforgeIntegration(BaseIntegration):
     BASE_URL = "https://api.curseforge.com/v1"
     API_KEY = get_settings().curseforge_api_key
 
-    async def get_mod(self, mod: CurseforgeMod) -> CompiledMod:
-        url = self.BASE_URL + f"/mods/{mod.mod_id}/files/{mod.file_id}"
+    async def get_mod(self, mod: IntegrationMod) -> CompiledMod:
+        url = self.BASE_URL + f"/mods/{mod.mod}/files/{mod.version}"
         headers = {'Accept': 'application/json', 'x-api-key': self.API_KEY}
 
         async with aiohttp.ClientSession() as session:
@@ -27,10 +24,7 @@ class CurseforgeIntegration(BaseIntegration):
                     raise Exception(f"{self.__repr__()} is empty")
                 return CompiledMod(file=data.get("fileName"), url=data.get("downloadUrl"))
 
-    async def extract_mods(self, instance: Instance) -> List[IntegrationMod]:
-        return instance.curseforge
-
 
 async def get_curseforge_integration(
         mod_cache_service: ModCacheService = Depends(get_mod_cache_service)) -> CurseforgeIntegration:
-    return CurseforgeIntegration(mod_cache_service)
+    return CurseforgeIntegration(mod_cache_service, IntegrationType.curseforge)
