8c9feef2f04db20acce18698b83e2bc596469a2f
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-11-28 18:17:25 +0400

    reimplemented mod service as content service class

---
+++ app/services/mod_content_service.py
@@ -0,0 +1,22 @@
+from fastapi.params import Depends
+
+from app.integrations.curseforge_integration import get_curseforge_integration, CurseforgeIntegration
+from app.integrations.modrinth_integration import get_modrinth_integration, ModrinthIntegration
+from app.repositories.mod_content_repository import ModContentRepository
+from app.repositories.mongo.mongo_mod_content_repository import MongoModContentRepository
+from app.services.content_service import ContentService
+
+
+class ModContentService(ContentService):
+    pass
+
+
+async def get_mod_content_service(repository: ModContentRepository = Depends(MongoModContentRepository),
+                                  modrinth_integration: ModrinthIntegration = Depends(get_modrinth_integration),
+                                  curseforge_integration: CurseforgeIntegration = Depends(
+                                      get_curseforge_integration)) -> ModContentService:
+    integrations = [
+        modrinth_integration,
+        curseforge_integration
+    ]
+    return ModContentService(repository, integrations)
