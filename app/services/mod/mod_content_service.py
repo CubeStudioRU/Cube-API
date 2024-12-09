6dfbc74cb8d70ac9a9865115ededdca1857e4212
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-12-09 12:30:49 +0400

    refactored content services along integrations_dependency

--- app/services/mod/mod_content_service.py
+++ app/services/mod/mod_content_service.py
@@ -1,8 +1,9 @@
+from typing import List
+
 from fastapi.params import Depends
 
-from app.integrations.cdn_integration import CdnIntegration, get_cdn_integration
-from app.integrations.curseforge_integration import get_curseforge_integration, CurseforgeIntegration
-from app.integrations.modrinth_integration import get_modrinth_integration, ModrinthIntegration
+from app.integrations.base_integration import BaseIntegration
+from app.integrations.integrations_dependency import get_integrations
 from app.repositories.mod.mod_content_repository import ModContentRepository
 from app.repositories.repositories_mongo.mod.mongo_mod_content_repository import MongoModContentRepository
 from app.services.content_service import ContentService
@@ -12,14 +13,8 @@ class ModContentService(ContentService):
     pass
 
 
-async def get_mod_content_service(repository: ModContentRepository = Depends(MongoModContentRepository),
-                                  modrinth_integration: ModrinthIntegration = Depends(get_modrinth_integration),
-                                  curseforge_integration: CurseforgeIntegration = Depends(
-                                      get_curseforge_integration),
-                                  cdn_integration: CdnIntegration = Depends(get_cdn_integration)) -> ModContentService:
-    integrations = [
-        modrinth_integration,
-        curseforge_integration,
-        cdn_integration
-    ]
+async def get_mod_content_service(
+        repository: ModContentRepository = Depends(MongoModContentRepository),
+        integrations: List[BaseIntegration] = Depends(get_integrations),
+) -> ModContentService:
     return ModContentService(repository, integrations)
--- app/services/resourcepack/resourcepack_content_service.py
+++ app/services/resourcepack/resourcepack_content_service.py
@@ -1,8 +1,9 @@
-from fastapi.params import Depends
+from typing import List
 
-from app.integrations.cdn_integration import CdnIntegration, get_cdn_integration
-from app.integrations.curseforge_integration import get_curseforge_integration, CurseforgeIntegration
-from app.integrations.modrinth_integration import get_modrinth_integration, ModrinthIntegration
+from fastapi import Depends
+
+from app.integrations.base_integration import BaseIntegration
+from app.integrations.integrations_dependency import get_integrations
 from app.repositories.repositories_mongo.resourcepack.mongo_resourcepack_content_repository import \
     MongoResourcepackContentRepository
 from app.repositories.resourcepack.resourcepack_content_repository import ResourcepackContentRepository
@@ -15,13 +16,6 @@ class ResourcepackContentService(ContentService):
 
 async def get_resourcepack_content_service(
         repository: ResourcepackContentRepository = Depends(MongoResourcepackContentRepository),
-        modrinth_integration: ModrinthIntegration = Depends(get_modrinth_integration),
-        curseforge_integration: CurseforgeIntegration = Depends(
-            get_curseforge_integration),
-        cdn_integration: CdnIntegration = Depends(get_cdn_integration)) -> ResourcepackContentService:
-    integrations = [
-        modrinth_integration,
-        curseforge_integration,
-        cdn_integration
-    ]
+        integrations: List[BaseIntegration] = Depends(get_integrations)
+) -> ResourcepackContentService:
     return ResourcepackContentService(repository, integrations)
